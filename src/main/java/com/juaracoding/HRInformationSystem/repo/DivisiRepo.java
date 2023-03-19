4e02f3d62e21bb0e45494ce73a7c65bd5ab8c89f
jejehyunlee <jefrisaputra989@gmail.com>
2023-03-19 13:44:08 +0700

    update HRIS

--- src/main/java/com/juaracoding/HRInformationSystem/repo/DivisiRepo.java
+++ src/main/java/com/juaracoding/HRInformationSystem/repo/DivisiRepo.java
@@ -1,5 +1,4 @@
 package com.juaracoding.HRInformationSystem.repo;
-<<<<<<< HEAD
 
 /*
 Created By IntelliJ IDEA 2022.1.3 (Community Edition)
@@ -28,20 +27,3 @@ public interface DivisiRepo extends JpaRepository<Divisi,Long> {
     Page<Divisi> findByIsDeleteAndIdDivisi(Pageable page , byte byteIsDelete, Long values);
 
 }
-=======
-/*
-Created By IntelliJ IDEA 2022.3.1 (Community Edition)
-Build #IC-223.8214.52, built on December 20, 2022
-@Author ASUS a.k.a. Aldi Cahya Ramadhan
-Java Developer
-Created on 09/03/2023 20:12
-@Last Modified 09/03/2023 20:12
-Version 1.0
-*/
-
-import com.juaracoding.HRInformationSystem.model.Divisi;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DivisiRepo extends JpaRepository<Divisi,Long> {
-}
->>>>>>> origin/master
